9ff7c364985ec089745614a83f00910dd369f1e1
Joseph Keiyuru <josephkeiyuru1@gmail.com>
2025-04-28 11:51:24 +0300

    Changed to new render URL

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/register",
+      "https://nemmoh-ecommerce-server.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://nemmoh-ecommerce-server.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,9 +44,9 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/logout",
+      "https://nemmoh-ecommerce-server.onrender.com/api/auth/logout",
       {},
-      {
+      {http://localhost:5000
         withCredentials: true,
       }
     );
@@ -60,7 +60,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "http://localhost:5000/api/auth/check-auth",
+      "https://nemmoh-ecommerce-server.onrender.com/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
