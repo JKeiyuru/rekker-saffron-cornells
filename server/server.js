933f8c9f34a94748e8e6259b72a66a167d38cd93
Joseph Keiyuru <josephkeiyuru1@gmail.com>
2025-06-12 09:10:49 +0300

    Fixed build fails

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 // Firebase Admin Initialization
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json"); // Ensure this file exists
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); // Ensure this file exists
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
